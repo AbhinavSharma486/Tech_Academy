aaea15ab58bd3a306912ae0deb25d8075174d9d0
ansari katib <ansarikatib647@gmail.com>
2025-06-21 19:25:03 +0530

    change in navbar and ladingpage cards

--- Frontend/src/assets/Logo/Logo.jsx
+++ Frontend/src/assets/Logo/Logo.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import LogoSvg from './LogoSvg'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
     <div className='flex gap-1 w-auto justify-center items-center'>
-      <LogoSvg
-        className=" w-[9vw] sm:w-[9vw] md:w-[7vw] lg:w-[3vw] h-[5vh] sm:h-[5vw] md:h-[5vw] lg:h-[4vh] rounded-2xl"
-        Textcolor={'blue'}
-      />
-
-      <span className='block text-[5vw] sm:text-[4vw]  md:text-[3vw] lg:text-[1.2rem] font-bold bg-gradient-to-r from-blue-900  to-blue-400 bg-clip-text text-transparent'>
-        Tech Academy
-      </span>
+      <Link to={'/'} className='flex items-center justify-center' >
+        <LogoSvg
+          className=" w-[9vw] sm:w-[9vw] md:w-[7vw] lg:w-[3vw] h-[5vh] sm:h-[5vw] md:h-[5vw] lg:h-[4vh] rounded-2xl"
+          Textcolor={'blue'}
+        />
+        <span className='block text-[5vw] sm:text-[4vw]  md:text-[3vw] lg:text-[1.2rem] font-bold bg-gradient-to-r from-blue-900  to-blue-400 bg-clip-text text-transparent'>
+          Tech Academy
+        </span>
+      </Link>
     </div>
   )
 }
--- Frontend/src/components/LandingPage/card/3DCard/HeroCard.jsx
+++ Frontend/src/components/LandingPage/card/3DCard/HeroCard.jsx
@@ -3,15 +3,14 @@ import image from '../../../../assets/LandingPageImage/HeroImages/mentor-image.w
 import { Link } from 'react-router-dom';
 import { AiOutlineArrowRight } from "react-icons/ai";
 
-
 const HeroCard = ({ src, title, des }) => {
 
   return (
-    <div className="flex items-center justify-center w-[100vw] sm:w-[80vw] md:w-[70vw] max-w-[90vw] lg:max-w-[45vw] xl:max-w-[35vw] h-[30vh] md:h-[35vh] xl:h-[30vh] bg-gray-100 rounded-2xl">
-      <div className="relative group w-full h-full p-4 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900 rounded-2xl flex items-center justify-between overflow-hidden transition-all duration-300 hover:shadow-xl">
+    <div className="flex items-center justify-center w-full sm:w-[80vw] md:w-[70vw] max-w-[90vw] lg:max-w-[45vw] xl:max-w-[35vw] h-[30vh] md:h-[35vh] xl:h-[30vh] bg-gray-100 rounded-2xl">
+      <div className="relative group w-full h-full p-4 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900 rounded-2xl flex flex-col md:flex-row items-center justify-between overflow-hidden transition-all duration-300 hover:shadow-xl">
 
         {/* Left: Text */}
-        <div className="flex flex-col justify-between py-5 h-full w-[60%] z-10">
+        <div className="flex flex-col justify-between py-5 gap-5 h-full w-full md:w-[60%] z-10">
           {/* Top: Heading and Paragraph */}
           <div>
             <h2 className="text-[4vw] sm:text-[3vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] text-white font-bold mb-1">
@@ -23,7 +22,7 @@ const HeroCard = ({ src, title, des }) => {
           </div>
 
           {/* Bottom: View Courses Link */}
-          <div className="w-fit my-5">
+          <div className="w-fit mt-auto mb-3">
             <Link to={src}>
               <span className="flex items-center justify-between text-[3vw] sm:text-[2vw] md:text-[2vw] lg:text-[1.7vw] xl:text-[1vw] px-4 py-2 rounded-2xl bg-white font-medium hover:bg-gray-200 transition">
                 View Courses&nbsp;
@@ -36,7 +35,7 @@ const HeroCard = ({ src, title, des }) => {
 
 
         {/* Right: Image */}
-        <div className="absolute bottom-0 right-4">
+        <div className="absolute bottom-0 right-4 md:static">
           <img
             src={image}
             alt="mentor"
