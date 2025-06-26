6f34d056abdb174d514aba4d220567f130f2e6d1
ay1878760 <ay1878760@gmail.com>
2025-06-26 17:40:43 +0530

    Add industryProject Section in dataAnalytics Course

--- Frontend/src/pages/DataAnalytics.jsx
+++ Frontend/src/pages/DataAnalytics.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HeroSection from "../components/DataAnalytics/HeroSection";
+import IndustryProjects from "../components/DataAnalytics/IndustryProjects";
 const DataAnalyticsPage = () => {
   return (
     <div className="">
         <HeroSection/>
+        <IndustryProjects/>
     </div>
   )
 }
